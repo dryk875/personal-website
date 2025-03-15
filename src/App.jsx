e21ba0de01dac3d3c8c5db5e9339a94b2e8749e1
damien <deryckdamien@gmail.com>
2025-03-15 15:03:02 +0100

    [DEL] url

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,8 @@ import AboutMe from "./AboutMe.jsx";
 import Header from "./components/Header.jsx";
 import Education from "./Education.jsx";
 import Experience from "./Experience.jsx";
-import useSectionObserver from './hooks/useSectionObserver';
 
 function App() {
-    const sections = ['aboutme', 'projects', 'education'];
-
-    useSectionObserver(sections);
-
-    useEffect(() => {
-        const path = window.location.pathname.slice(1); // Get the section name from the URL
-        if (sections.includes(path)) {
-            const element = document.getElementById(path);
-            if (element) {
-                element.scrollIntoView({ behavior: 'smooth' });
-            }
-        }
-    }, [sections]);
-
     return (
         <>
             <Header />
