06c24712b724763433138a721528d30a8ffec29d
damien <deryckdamien@gmail.com>
2025-06-02 23:08:53 +0200

    [FIX] indent

--- src/components/Return.jsx
+++ src/components/Return.jsx
@@ -1,42 +1,39 @@
 import React from "react";
-                                import { useNavigate, useLocation } from 'react-router-dom';
-                                import '../style/Return.css';
+import { useNavigate, useLocation } from 'react-router-dom';
+import '../style/Return.css';
 
-                                const LANG_CODES = ['en', 'es', 'fr', 'de', 'it', 'pt'];
+const LANG_CODES = ['en', 'es', 'fr', 'de', 'it', 'pt'];
 
-                                function Return() {
-                                    const navigate = useNavigate();
-                                    const location = useLocation();
+function Return() {
+    const navigate = useNavigate();
+    const location = useLocation();
 
-                                    const handleReturn = () => {
-                                        const pathSegments = location.pathname.split('/').filter(Boolean);
-                                        let lang = null;
+    const handleReturn = () => {
+        const pathSegments = location.pathname.split('/').filter(Boolean);
+        let lang = null;
 
-                                        if (LANG_CODES.includes(pathSegments[pathSegments.length - 1])) {
-                                            lang = pathSegments.pop();
-                                            if (pathSegments.length > 0) {
-                                                pathSegments.pop();
-                                            }
-                                        } else if (pathSegments.length > 0) {
-                                            pathSegments.pop();
-                                        }
+        if (LANG_CODES.includes(pathSegments[pathSegments.length - 1])) {
+            lang = pathSegments.pop();
+            if (pathSegments.length > 0) {
+                pathSegments.pop();
+            }
+        } else if (pathSegments.length > 0) {
+            pathSegments.pop();
+        }
 
-                                        let newPath = pathSegments.length ? '/' + pathSegments.join('/') : '';
-                                        if (lang) {
-                                            newPath += newPath ? '/' + lang : '/' + lang;
-                                        }
+        let newPath = pathSegments.length ? '/' + pathSegments.join('/') : '';
+        if (lang) {
+            newPath += newPath ? '/' + lang : '/' + lang;
+        }
 
-                                        // If newPath is empty, go to /[lang] if lang exists, else /
-                                        navigate(newPath || (lang ? '/' + lang : '/'));
-                                    };
+        navigate(newPath || (lang ? '/' + lang : '/'));
+    };
 
-                                    return (
-                                        <div className="ReturnHome">
-                                            <button onClick={handleReturn} className="return_button">
-                                                ←
-                                            </button>
-                                        </div>
-                                    );
-                                }
+    return (
+        <div className="ReturnHome">
+            <button onClick={handleReturn} className="return_button">←</button>
+        </div>
+    );
+}
 
-                                export default Return;
+export default Return;
