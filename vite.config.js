a2b6f6d1aab93a79cb5a69fb985f3298d817fea7
damien <deryckdamien@gmail.com>
2025-06-02 15:05:56 +0200

    [FIX] links

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 // vite.config.js
 export default defineConfig({
-  base: './', // ou '/' selon où tu héberges
+  base: '/',
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
 });
